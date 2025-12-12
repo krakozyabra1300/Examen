614b18ebf8c17c5ac28c5ffba28b493c34826cc9
krakozyabra1300 <163377134+krakozyabra1300@users.noreply.github.com>
2025-12-13 00:20:17 +1000

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,31 @@
+<?php
+if (!isset($_COOKIE['User'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$user = htmlspecialchars($_COOKIE['User']);
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8" />
+    <title>Gunya</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="hello">
+                            Привет, <?php echo $user; ?>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
